e0e03c25f84f0f524a74b8cd65f7c74d3fe81462
francomonja <51922845+francomonja@users.noreply.github.com>
2023-09-11 20:02:53 -0300

    remove commented lines

--- lib/core/services/products_service.dart
+++ lib/core/services/products_service.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:uuid/uuid.dart';
@@ -46,21 +44,6 @@ class ProductsService {
     tempProduct.sort((a, b) => a.name.compareTo(b.name));
     return tempProduct;
   }
-  // Future<List<Product>> loadProducts() async {
-  //   List<Product> tempProduct = [];
-  //   final url = Uri.https(_baseUrl, 'products.json');
-  //   final resp = await http.get(url);
-  //   try {
-  //     final Map<String, dynamic> productsMap = json.decode(resp.body);
-  //     productsMap.forEach((key, value) {
-  //       tempProduct.add(Product.fromJson(value));
-  //     });
-  //   } catch (error) {
-  //     print('Error: $error');
-  //   }
-  //   tempProduct.sort((a, b) => a.name.compareTo(b.name));
-  //   return tempProduct;
-  // }
 
   Future saveOrCreateProduct(Product product) async {
     if (product.id == null) {
@@ -93,24 +76,6 @@ class ProductsService {
     return product.id!;
   }
 
-// Map<String, dynamic> pictureMap = {}; // Crear un mapa vacío
-// for (int i = 0; i < pictureList.length; i++) {
-//   pictureMap['picture$i'] = pictureList[i]; // Asignar cada URL como valor del mapa usando la clave 'picture$i'
-// }
-  // void updateSelectedProductImage(String path, int i) {
-  //   selectedProduct!.picture!['picture$i'] = path;
-  //   int long = selectedProduct!.picture!.length;
-  //   newPictureFile.clear();
-  //   for (var i = 0; i < long; i++) {
-  //     newPictureFile.add(File(selectedProduct!.picture!['picture$i']));
-  //   }
-  //   if (newPictureFile.length > i) {
-  //     newPictureFile.insert(i, File(path));
-  //   } else {
-  //     newPictureFile.add(File(path));
-  //   }
-  //   print(newPictureFile);
-  // }
   void updateSelectedProductImage(String path, int i) {
     if (selectedProduct!.picture != null) {
       // Actualiza la imagen en selectedProduct
@@ -162,10 +127,6 @@ class ProductsService {
     // Modificar el mapa selectedProduct!.picture con las URLs subidas
     final pictureMap = <String, dynamic>{};
 
-    // for (var i = 0; i < decodedDataList.length; i++) {
-    //   pictureMap['picture$i'] = decodedDataList[i];
-    // }
-
     int pictureIndex = 0; // Usar un índice incremental
     for (var imageUrl in decodedDataList) {
       pictureMap['picture$pictureIndex'] = imageUrl;
@@ -184,32 +145,6 @@ class ProductsService {
 
     return decodedDataList;
   }
-
-  // Future<List<String?>?> uploadImage() async {
-  //   if (newPictureFile.isEmpty) return null;
-  //   List<String> decodedDataList = [];
-
-  //   final url = Uri.parse(
-  //       'https://api.cloudinary.com/v1_1/dgagjc77g/image/upload?upload_preset=puyyq9zb');
-  //   final imageUploadRequest = http.MultipartRequest('POST', url);
-
-  //   for (var i = 0; i < newPictureFile.length; i++) {
-  //     final file =
-  //         await http.MultipartFile.fromPath('file', newPictureFile[i]!.path);
-  //     imageUploadRequest.files.add(file);
-  //     final streamResponse = await imageUploadRequest.send();
-  //     final resp = await http.Response.fromStream(streamResponse);
-  //     if (resp.statusCode != 200 && resp.statusCode != 201) {
-  //       print('algo salio mal');
-  //       print(resp.body);
-  //       return null;
-  //     }
-  //     final decodedData = json.decode(resp.body);
-  //     decodedDataList.add(decodedData);
-  //   }
-  //   newPictureFile = [];
-  //   return decodedDataList;
-  // }
 
   onDelete(id) async {
     try {
